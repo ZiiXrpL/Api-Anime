d5581bc0f046aa505034c3a3944385e18fc712de
ZiiXrpL <greeytchn@gmail.com>
2026-08-06 11:40:11 +0700

    Hapus tuntas folder scraper Otakudesu/Samehadaku yang tertinggal

--- src/scrapers/samehadaku/stream.ts
+++
@@ -1,7 +0,0 @@
-import { StreamServer } from '../../interfaces/anime.interface';
-import { getEpisodeDetail } from './episode';
-
-export async function getStreamServers(episodeSlug: string): Promise<StreamServer[]> {
-  const episode = await getEpisodeDetail(episodeSlug);
-  return episode.streamServers;
-}
